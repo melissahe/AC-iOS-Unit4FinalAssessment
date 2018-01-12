360bd4f04d5d692c825ff7bf96a7a03526eec353
Melissa He <melissahe@ac.c4q.nyc>
2018-01-12 12:41:18 -0500

    Add save button in settings

--- Unit4FinalAssessment-StudentVersion/Models/Animation.swift
+++ Unit4FinalAssessment-StudentVersion/Models/Animation.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 //to set up - should be the data source variable for the picker view
+//animations should always have autoreverse so they return to the center
+
+//should have group animations - each using a part of these values - keypath will be decided in those values
 
 struct Animation: Codable {
+    let name: String
     let widthMultiplier: CGFloat
     let heightMultiplier: CGFloat
     let horizontalOffset: CGFloat
     let verticalOffset: CGFloat
-    let numberOfFlips: CGFloat //this would be the repeat times property
+    let numberOfFlips: Float //this would be the repeat times property for a flip animation
+    
+    //add other properties? - Extra Credit?
+//    let duration: Double
+//    let delay???
+
 }
 
--- Unit4FinalAssessment-StudentVersion/View Controllers/SettingsViewController.swift
+++ Unit4FinalAssessment-StudentVersion/View Controllers/SettingsViewController.swift
@@ -28,22 +28,43 @@ struct AnimationProperty {
     let startingStepperVal: Double
 }
 
+protocol SettingsViewControllerDelegate: class {
+    func addAnimation(fromSettings animation: Animation)
+}
+
 class SettingsViewController: UIViewController {
 
+    var delegate: SettingsViewControllerDelegate?
+    
     var properties: [[AnimationProperty]] =
         [
-            [AnimationProperty(name: .widthMultiplier, stepperMin: 0.1, stepperMax: 2.0, stepperIncrement: 0.1, startingStepperVal: 0.0),
-             AnimationProperty(name: .heightMultiplier, stepperMin: 0.1, stepperMax: 2.0, stepperIncrement: 0.1, startingStepperVal: 0.0)
+            [AnimationProperty(name: .widthMultiplier, stepperMin: 0.1, stepperMax: 2.0, stepperIncrement: 0.1, startingStepperVal: 1.0),
+             AnimationProperty(name: .heightMultiplier, stepperMin: 0.1, stepperMax: 2.0, stepperIncrement: 0.1, startingStepperVal: 1.0)
             ],
             [AnimationProperty(name: .horizontalOffset, stepperMin: -100.0, stepperMax: 100.0, stepperIncrement: 20.0, startingStepperVal: 0.0), AnimationProperty(name: .verticalOffset, stepperMin: -100.0, stepperMax: 100.0, stepperIncrement: 20.0, startingStepperVal: 0.0)
             ],
             [AnimationProperty(name: .numberOfFlips, stepperMin: 0, stepperMax: 10.0, stepperIncrement: 1.0, startingStepperVal: 0.0)]
     ]
     
+    lazy var tableView: UITableView = {
+        let tv = UITableView()
+        tv.dataSource = self
+        tv.delegate = self
+
+        //I didn't register a cell with the table view here because I didn't need a cell with a reusable identifier - at least, not yet
+        
+        //check extra credit - 2 additional animations dimensions? - is that two extra settings?
+            //possible settings - maybe duration and delay? - should be double
+            //this would need to change my animation model
+        
+        return tv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(tableView)
-        navigationItem.title = "Settings"
+        
+        setUpNavigationItems()
         layoutTableView()
     }
     
@@ -55,13 +76,39 @@ class SettingsViewController: UIViewController {
         tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
     }
     
-    lazy var tableView: UITableView = {
-        let tv = UITableView()
-        tv.dataSource = self
-        tv.delegate = self
-        //TO DO: Register your subclass - only if you need a reuse identifier i think; since there is a limited amount of cells, maybe i don't need one?
-        return tv
-    }()
+    func setUpNavigationItems() {
+        navigationItem.title = "Settings"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addSettingsAnimation))
+    }
+    
+    @objc func addSettingsAnimation() {
+        //set up an alert that presents (should have title, text label, and options below; cancel on left, OK on right; completion handler of okay should trigger save using delegate), and takes in a name
+        
+        let alertController = UIAlertController(title: "Add Setting", message: "Give your custom settings a name!", preferredStyle: .alert)
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Name your settings here"
+            textField.textAlignment = .center
+        }
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+            
+            let heightCell = self.tableView.cellForRow(at: IndexPath(row: 0, section: 0)) as! SettingsTableViewCell
+            let widthCell = self.tableView.cellForRow(at: IndexPath(row: 1, section: 0)) as! SettingsTableViewCell
+            let horizontalCell = self.tableView.cellForRow(at: IndexPath(row: 0, section: 1)) as! SettingsTableViewCell
+            let verticalCell = self.tableView.cellForRow(at: IndexPath(row: 1, section: 1)) as! SettingsTableViewCell
+            let flipsCell = self.tableView.cellForRow(at: IndexPath(row: 0, section: 2)) as! SettingsTableViewCell
+            
+            let animation = Animation(name: alertController.textFields![0].text!, widthMultiplier: CGFloat(widthCell.stepper.value), heightMultiplier: CGFloat(heightCell.stepper.value), horizontalOffset: CGFloat(horizontalCell.stepper.value), verticalOffset: CGFloat(verticalCell.stepper.value), numberOfFlips: Float(flipsCell.stepper.value))
+            
+            self.delegate?.addAnimation(fromSettings: animation)
+        }))
+
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
     
 }
 
